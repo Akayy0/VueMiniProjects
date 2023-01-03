1ec8468cf6e914a43defdac8abd098032a2bc024
João Vitor <joaovitorotoni6@gmail.com>
2023-01-03 18:06:06 -0300

    Training

--- basic-counter/script.js
+++ basic-counter/script.js
@@ -1,8 +1,23 @@
 const app = Vue.createApp({
     data() {
         return {
-            number: 0  
+            number: 0,
+            id : 0
         }
+    },
+
+    methods: {
+        increase(){
+
+            if (this.id == null || this.id == undefined) {
+                this.id = 0
+              }        
+            this.id += 1
+            console.log(this.id)
+
+            this.number++
+        }
+        
     },
 })
 
